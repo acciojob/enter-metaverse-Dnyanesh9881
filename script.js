ab3dc9c8b38ca7810c238e4e1de20a47535f77d5
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-21 02:22:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   const button=document.getElementById("enterBtn");
 
   button.addEventListener("click", ()=>{
-	  let h1=documnet.create("h1");
+	  let h1=document.createElement("h1");
 	  h1.id="status";
 	  h1.innerText="Metaverse Entered";
 	  document.body.insertBefore(h1,ptag);
